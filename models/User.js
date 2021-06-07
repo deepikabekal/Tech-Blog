a9f9be6e88803044ad8fbe7807dbc951809e11e9
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-07 15:18:58 +0400

    user model created

---
+++ models/User.js
@@ -0,0 +1,70 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+// create User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkpassword(loginPw)
+    {
+        return bcrypt.compare(loginPw, this.password);
+    }
+}
+
+//define table columns and configurations
+
+User.init(
+    {
+        //table column defination
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type : DataTypes.STRING,
+            allowNull : false,
+            unique : true,
+            validate : {
+                isEmail : true
+            }
+        },
+        // define a password column
+        password: {
+            type : DataTypes.STRING,
+            allowNull : false,
+            validate : {
+                len : [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+            //table configuration options
+            sequelize,
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'user'
+    }
+);
+
+module.exports = User;
