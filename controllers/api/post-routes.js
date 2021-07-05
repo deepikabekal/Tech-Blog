b9ff63d23e219aba61ea53603f26f1078b95b885
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-06 03:28:57 +0400

    auth and post route for posts created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Post} = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET /api/posts
 router.get('/', (req, res) => {
@@ -9,6 +10,18 @@ router.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+      title: req.body.title,
+      content: req.body.content
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
