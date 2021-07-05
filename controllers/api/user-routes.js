72101cd0817a9c308f53f96dda0e9fadf164d308
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-05 19:27:09 +0400

    get user by id route created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,3 +13,24 @@ router.get('/', (req, res) => {
     });
 });
 
+//GET /api/users/:id
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
