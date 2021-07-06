3b49358cd033f4e3e2139fbea2cc74b9025c3e2b
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-06 04:51:38 +0400

    fixed connection, added main handlebar code

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,9 @@ const Sequelize = require ('sequelize');
 
 require('dontenv').config();
 
-const sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host : 'localhost',
     dialect : 'mysql', 
     port : 3306
