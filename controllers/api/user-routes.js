173852399e7e0fa9e00a67ec8b922d8169d7762d
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-16 18:05:40 +0400

    post route created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -115,7 +115,7 @@ router.post("/login", (req, res) => {
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout',withAuth, (req, res) => {
     if (req.session.loggedIn) {
       req.session.destroy(() => {
         res.status(204).end();
@@ -151,7 +151,7 @@ router.put('/:id', (req, res) => {
 });
 
 //DELETE /api/users/:id
-router.delete('/:id', (req, res) => {
+router.delete('/:id',withAuth, (req, res) => {
     User.destroy({
       where: {
         id: req.params.id
